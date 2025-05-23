8b200b1a9a592a8ba335a4b8dd1f1cf3798c21a9
Victor Qu <Victor.X.Qu@newegg.com>
2025-05-23 16:42:06 +0800

    UseSni

--- src/VKProxy.Admin/vkproxy.admin.client/src/page/listenDetail.vue
+++ src/VKProxy.Admin/vkproxy.admin.client/src/page/listenDetail.vue
@@ -2,10 +2,10 @@
   <el-form ref="formRef" :model="form" :rules="rules" label-width="auto">
 
     <el-form-item :label="$t('key')" prop="Key" :rules="checkName" v-if="isNew">
-      <el-input v-model="form.Key"  />
+      <el-input v-model="form.Key" />
     </el-form-item>
     <el-form-item :label="$t('key')" prop="Key" v-else>
-      <el-text >{{ form.Key }}</el-text>
+      <el-text>{{ form.Key }}</el-text>
     </el-form-item>
 
     <el-form-item :label="$t('protocols')" prop="Protocols">
@@ -13,9 +13,22 @@
     </el-form-item>
 
     <el-form-item :label="$t('address')" prop="Address">
-      <ipAddress v-model="form.Address" ref="address"/>
+      <ipAddress v-model="form.Address" ref="address" />
+    </el-form-item>
+    <el-form-item prop="UseSni">
+      <template #label>
+        <span>{{ $t('UseSni') }}</span>
+        <el-tooltip placement="top">
+          <template #content> {{ $t('UseSniTip') }} </template>
+          <el-icon>
+            <QuestionFilled />
+          </el-icon>
+        </el-tooltip>
+      </template>
+      <el-checkbox v-model="form.UseSni" @change="(v) => { if (v) { form.SniId = null; form.RouteId = null; } }" />
     </el-form-item>
     <!--todo sni and route-->
+    {{ form }}
     <el-form-item>
       <template #label>
         <el-button type="primary" @click="submitForm(formRef)">
@@ -37,6 +50,8 @@ import { useI18n } from 'vue-i18n';
 import { protocolsSelect, ipAddress } from '../components'
 import { protocolsValidator } from '../service/validators'
 import { storageService } from '../service/storage'
+import { QuestionFilled } from '@element-plus/icons-vue'
+import { GatewayProtocols } from '../ets/GatewayProtocols';
 
 const { t } = useI18n({
   useScope: 'global'
@@ -60,8 +75,25 @@ const form = reactive(new ListenData({}))
 const rules = reactive<FormRules<ListenData>>({
   Key: [{ required: true, message: () => t('required'), trigger: 'blur' },
   { min: 1, message: () => t('requiredLength') + '1', trigger: 'blur' },],
-  Protocols: [{ required: true, message: () => t('required'), trigger: 'change' }, protocolsValidator],
-  Address: [{ required: true, message: () => t('required'), trigger: 'change' }]
+  Protocols: [{ required: true, message: () => t('required'), trigger: 'change' }, protocolsValidator, {
+    validator: (rule: any, value: any, callback: any) => {
+      if (form.UseSni && (value & GatewayProtocols.HTTP1 || value & GatewayProtocols.HTTP2 || value & GatewayProtocols.HTTP3 || value & GatewayProtocols.HTTP3)) {
+        callback(new Error(t('UseSniError')))
+      } else {
+        callback()
+      }
+    }, trigger: 'blur'
+  }],
+  Address: [{ required: true, message: () => t('required'), trigger: 'change' }],
+  UseSni: [{
+    validator: (rule: any, value: any, callback: any) => {
+      if (value && (form.Protocols & GatewayProtocols.HTTP1 || form.Protocols & GatewayProtocols.HTTP2 || form.Protocols & GatewayProtocols.HTTP3 || form.Protocols & GatewayProtocols.HTTP3)) {
+        callback(new Error(t('UseSniError')))
+      } else {
+        callback()
+      }
+    }, trigger: 'blur'
+  }]
 })
 
 watchEffect(() => {
@@ -76,20 +108,20 @@ watchEffect(() => {
 
 const submitForm = async (formEl: FormInstance | undefined) => {
   let invalid = false
-   for (const element of forms) {
-        if (element && element.value) {
-            const v = !await element.value.validate();
-            if (!invalid) {
-                invalid = v
-            }
-        }
+  for (const element of forms) {
+    if (element && element.value) {
+      const v = !await element.value.validate();
+      if (!invalid) {
+        invalid = v
+      }
     }
-  if (invalid || !formEl || !await formEl.validate().catch(() => false)) {
-      ElMessage.error(t('wrongSave'))
-      return
   }
- var r = await storageService.updateListen(form);
- (props.done as any)()
+  if (invalid || !formEl || !await formEl.validate().catch(() => false)) {
+    ElMessage.error(t('wrongSave'))
+    return
+  }
+  var r = await storageService.updateListen(form);
+  (props.done as any)()
 }
 
 const checkName = [{
@@ -98,7 +130,7 @@ const checkName = [{
       callback(new Error(t('required')))
     } else {
       storageService.existsListen(value).then(i => {
-        if(i) {
+        if (i) {
           callback(new Error(t('alreadyExists')))
         } else {
           callback()
