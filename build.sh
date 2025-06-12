74c91900ba943a2130ded301f401db7739e8e47a
Victor Qu <Victor.X.Qu@newegg.com>
2025-06-12 16:11:16 +0800

    remove test file

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf src/VKProxy.Admin/vkproxy.admin.client/dist
-rm -rf src/VKProxy.Admin/VKProxy.Admin.Server/bin/Release
-cd src/VKProxy.Admin/vkproxy.admin.client
-npm run build
-cd ../VKProxy.Admin.Server
-dotnet build --configuration Release
-cd ../
-cp -r vkproxy.admin.client/dist VKProxy.Admin.Server/bin/Release/net9.0/wwwroot
--- src/VKProxy.Admin/bd.sh
+++
@@ -1,2 +0,0 @@
-
-docker build -t vkproxy/ui:0.0.0.1 .
